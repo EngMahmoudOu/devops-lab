4b507314c6596f07ea643589985fa9e7c00fcb39
EngMahmoudOu <mahmoudoudah@outlook.com>
2026-07-22 05:27:02 +0000

    Day 2 of DevOps

--- bash/hello.sh
+++ bash/hello.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
-
-echo "DevOps"
-echo "Date:"
-date
+echo "DevOps Practice"
+echo "what is your name?"
+read NAME
+echo ""
+echo "Today is: $(date)"
+echo "Current user: $(whoami)"
+echo "Current directory: $(pwd)"
