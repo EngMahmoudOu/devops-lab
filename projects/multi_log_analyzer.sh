b35841667c3c95f033a6bec06ad2dac28411b040
EngMahmoudOu <mahmoudoudah@outlook.com>
2026-07-27 10:26:29 +0000

    multi_log_analyzer.sh

--- projects/multi_log_analyzer.sh
+++ projects/multi_log_analyzer.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-for file in logs/*.log
+for file in logs/*.log 
 do
 echo "File: $(basename "$file")"
-echo "Total lines: $(wc -l < "$file")"
+echo "Total lines: $(wc -l < $file)"
 echo "Total Errors: $(grep -i "ERROR" "$file" |wc -l)"
-echo "Total Warning: $(grep -i "Warning" $file |wc -l)"
-echo "Total Info: $(grep -i "Info" "$file" |wc -l)"
-echo "==================================="
+echo "Total Warnings: $(grep -i "WARNING" "$file" |wc -l)"
+echo "Total Info: $(grep -i "INFO" "$file" |wc -l)"
+echo "=========================="
 done
